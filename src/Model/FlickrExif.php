2cea408dfe828eb4d37dc039ca49de36f94e2a34
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-03-29 00:00:56 +0700

    Initial checkin

---
+++ src/Model/FlickrExif.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Suilven\Flickr\Model;
+
+
+class FlickrExif extends DataObject {
+
+	static $db = array(
+		'TagSpace' => 'Varchar',
+		'Tag' => 'Varchar',
+		'Label' => 'Varchar',
+		'Raw' => 'Varchar',
+		'TagSpaceID' => 'Int'
+	);
+
+	 static $belongs_many_many = array(
+		'FlickrPhotos' => 'FlickrPhoto'
+	 );
+
+	 static $has_one = array(
+		'FlickrPhoto' => 'FlickrPhoto'
+	);
+
+}
